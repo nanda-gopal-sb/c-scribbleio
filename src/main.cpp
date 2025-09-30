1c4188fcb8eb05fd7b52027e42ea2e26d2719394
nanda-gopal-sb <nandagopal.nsb@gmail.com>
2025-09-30 16:04:09 +0530

    adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
-
 #include <imgui-SFML.h>
 #include <imgui.h>
 
@@ -10,7 +9,6 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1280, 720), "ImGui + SFML = <3");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
-
     sf::Clock deltaClock;
     while (window.isOpen())
     {
@@ -18,23 +16,17 @@ int main()
         while (window.pollEvent(event))
         {
             ImGui::SFML::ProcessEvent(event);
-
             if (event.type == sf::Event::Closed)
             {
                 window.close();
             }
         }
-
         ImGui::SFML::Update(window, deltaClock.restart());
-
         ImGui::ShowDemoWindow();
-
         window.clear();
         ImGui::SFML::Render(window);
         window.display();
     }
-
     ImGui::SFML::Shutdown();
-
     return 0;
 }
